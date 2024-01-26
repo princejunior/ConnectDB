896507b24e71484ba2fa33ae2195680d9ece33f7
Elijah Elliott <elliott.t.elijah@gmail.com>
2024-01-26 15:34:44 -0700

    Initial Commit

---
+++ ConnectDB/ConnectDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectDBApp.swift
+//  ConnectDB
+//
+//  Created by Elijah Elliott on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct ConnectDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
